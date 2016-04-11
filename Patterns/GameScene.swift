f7d2c3cbf6d828ece6e2147f549a51155416b3b0
Kristian Våge <github@krisvage.com>
2016-04-11 13:53:08 +0200

    Adds Alamofire and SwiftyJSON, and basic API call

--- Patterns/GameScene.swift
+++ Patterns/GameScene.swift
@@ -7,6 +7,8 @@
 //
 
 import SpriteKit
+import Alamofire
+import SwiftyJSON
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
@@ -15,10 +17,31 @@ class GameScene: SKScene {
         myLabel.text = "Hello, World!"
         myLabel.fontSize = 45
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
+
         self.addChild(myLabel)
+
+        let api_location = "http://recallo.noip.me/api/users/me" /// "http://82.196.0.219.xip.io/api/users/me"
+        let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImtyaXN2YWdlIiwiaWF0IjoxNDYwMzY4MjE2fQ.H5dmE_pBGSTzaE08Yk5_aZgR_lNia8jGZsL9zd2yjG0"
+
+        let headers = ["x-access-token": token]
+
+        Alamofire.request(.GET, api_location, headers: headers)
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.response?.statusCode)
+                print(response.data)     // server data
+
+                print(response.result)   // result of response serialization
+
+                if let json = response.result.value {
+                    print("JSON: \(json)")
+                    let parsed_json = JSON(json)
+                    print("message \(parsed_json["message"].stringValue)")
+                }
+        }
     }
-    
+
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
