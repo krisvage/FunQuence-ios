5dccb74214938ee5f39ffe93399487f6f251ce62
Kristian Våge <github@krisvage.com>
2016-04-12 13:53:25 +0200

    Implement Users#register api route

--- Patterns/Users.swift
+++ Patterns/Users.swift
@@ -13,15 +13,40 @@ import SwiftyJSON
 class Users {
     static let basePath = "http://recallo.noip.me/api"
     static let loginRoute = Route(path: basePath + "/login", method: Method.POST)
+    static let registerRoute = Route(path: basePath + "/users", method: Method.POST)
     static let myUserRoute = Route(path: basePath + "/users/me", method: Method.GET)
     
     static func login(username: String, password: String, completionHandler: (String?, String?, String?) -> ()) {
-        Alamofire.request(loginRoute!.method, loginRoute!.path, parameters: ["username": username, "password": password])
+        let params = ["username": username, "password": password]
+        
+        Alamofire.request(loginRoute!.method, loginRoute!.path, parameters: params)
             .responseJSON { response in
                 switch response.result {
                 case .Success(_):
                     if let json = response.result.value {
                         let parsed_json = JSON(json)
+                        if parsed_json["error"] == nil {
+                            completionHandler(parsed_json["token"].stringValue, parsed_json["message"].stringValue, nil)
+                        } else {
+                            completionHandler(nil, nil, parsed_json["error"].stringValue)
+                        }
+                    }
+                case .Failure(_):
+                    completionHandler(nil, nil, "failed request")
+                }
+        }
+    }
+    
+    static func register(username: String, email: String, password: String, completionHandler: (String?, String?, String?) -> ()) {
+        let params = ["username": username, "email": email, "password": password]
+        
+        Alamofire.request(registerRoute!.method, loginRoute!.path, parameters: params)
+            .responseJSON { response in
+                switch response.result {
+                case .Success(_):
+                    if let json = response.result.value {
+                        let parsed_json = JSON(json)
+                        
                         if parsed_json["error"] == nil {
                             completionHandler(parsed_json["token"].stringValue, parsed_json["message"].stringValue, nil)
                         } else {
